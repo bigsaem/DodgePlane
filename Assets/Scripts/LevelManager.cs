6f537f5e62e798b7d988fcf514a456c542f312b2
pakaratAriya <pakarat_ariya@hotmail.com>
2018-09-10 13:15:58 -0700

    implemented pause & resume function

--- Assets/Scripts/LevelManager.cs
+++ Assets/Scripts/LevelManager.cs
@@ -1,6 +1,7 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class LevelManager : MonoBehaviour {
 
@@ -11,7 +12,7 @@ public class LevelManager : MonoBehaviour {
 
     public void QuitRequest()
     {
-        Application.Quit();
+        SceneManager.LoadScene("Start");
     }
 
     public void LoadNextLevel()
---
+++ Assets/Scripts/PauseManager.cs
@@ -0,0 +1,59 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
+public class PauseManager : MonoBehaviour {
+
+    public Image panel;
+    public Button pauseBtn;
+	
+	void Update () {
+        if (Input.GetKeyDown(KeyCode.P))
+        {
+            if(Time.timeScale != 0)
+            {
+                Time.timeScale = 0;
+            }
+            else
+            {
+                Time.timeScale = 1;
+            }
+            
+        }
+	}
+
+    public void Pause()
+    {
+        Time.timeScale = 0;
+        if(pauseBtn && panel)
+        {
+            pauseBtn.gameObject.SetActive(false);
+            panel.gameObject.SetActive(true);
+        }
+            
+    }
+
+    public void Resume()
+    {
+        Time.timeScale = 1;
+        if (pauseBtn && panel)
+        {
+            pauseBtn.gameObject.SetActive(true);
+            panel.gameObject.SetActive(false);
+        }
+    }
+
+    public void Restart()
+    {
+        Time.timeScale = 1;
+        SceneManager.LoadScene("GameScene");
+    }
+
+    public void Quit()
+    {
+        Time.timeScale = 1;
+        SceneManager.LoadScene("Start");
+    }
+}
